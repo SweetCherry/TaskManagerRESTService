c093ea69f8721a587456536aa7ee337057258a16
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-25 00:29:55 +0200

    Added better error message

--- taskManagerService/src/main/java/com/cherries/ppmtool/Web/ProjectController.java
+++ taskManagerService/src/main/java/com/cherries/ppmtool/Web/ProjectController.java
@@ -5,12 +5,17 @@ import com.cherries.ppmtool.domain.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,7 +24,15 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
         //TODO add unhappy path
         Project project1 = projectService.savOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);

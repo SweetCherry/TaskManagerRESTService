a176132ddb64fc7a9fd66865722ed2eb2d36d262
Artur Wiśniewski <artwis1998@gmail.com>
2020-06-01 18:43:51 +0200

    Added excpetion handling for project not found exception

--- taskManagerService/src/main/java/com/cherries/ppmtool/Services/ProjectTaskService.java
+++ taskManagerService/src/main/java/com/cherries/ppmtool/Services/ProjectTaskService.java
@@ -3,6 +3,7 @@ package com.cherries.ppmtool.Services;
 import com.cherries.ppmtool.domain.Backlog;
 import com.cherries.ppmtool.domain.Project;
 import com.cherries.ppmtool.domain.ProjectTask;
+import com.cherries.ppmtool.exceptions.ProjectNotFoundException;
 import com.cherries.ppmtool.repositories.BacklogRepository;
 import com.cherries.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,26 +19,29 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
         //PTs to be added to specific project, not null
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        //set the bl to pt
-        projectTask.setBacklog(backlog);
-        //sequence
-        Integer backlogSequence = backlog.getPTSequence();
-        backlogSequence++;
-        //Add sedquence to project task
-        projectTask.setProjectSequence(projectIdentifier+ "-" + backlogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
-        //priority
-        if( projectTask.getPriority() == null || projectTask.getPriority() == 0){
-            projectTask.setPriority(3);
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            //set the bl to pt
+            projectTask.setBacklog(backlog);
+            //sequence
+            Integer backlogSequence = backlog.getPTSequence();
+            backlogSequence++;
+            //Add sedquence to project task
+            projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
+            //priority
+            if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+                projectTask.setPriority(3);
+            }
+            if (projectTask.getStatus() == null || projectTask.getStatus().equals("")) {
+                projectTask.setStatus("TO_DO");
+            }
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project ID '" + projectIdentifier.toUpperCase() + "' doesn't exists");
         }
-        if( projectTask.getStatus()== null || projectTask.getStatus().equals("")){
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public List<ProjectTask> findBacklogById(String id) {
---
+++ taskManagerService/src/main/java/com/cherries/ppmtool/exceptions/ProjectNotFoundExceptionResponse.java
@@ -0,0 +1,12 @@
+package com.cherries.ppmtool.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProjectNotFoundExceptionResponse {
+    private String projectNotFound;
+}
